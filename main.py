1a4567ab2e998168e1de76395fd822ba4e392efb
nabeel6223 <nabeelmd14@yahoo.com>
2026-02-07 09:46:09 +0530

    `Added root endpoint and uvicorn run configuration`

--- main.py
+++ main.py
@@ -256,3 +256,18 @@ def login(payload: LoginRequest = Body(...)):
             "message": "Failed to login. Please try again.",
             "token": None
         }
+
+
+# Root endpoint
+@app.get("/")
+def root():
+    return {"message": "HRMS API Running"}
+
+# --------------------
+# Run with uvicorn
+if __name__ == "__main__":
+    import os
+    import uvicorn
+
+    port = int(os.environ.get("PORT", 9090))  # Render sets this automatically
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
